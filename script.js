22b7f67bcc50400ae1350369ee396252737c4fba
Claudionei1995 <claudionei0580@gmail.com>
2024-04-05 22:01:21 -0300

    conversor finalizado

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const butt = document.querySelector(".but")
+const btnbutoon = document.getElementById("but")
+const currenselect = document.querySelector(".selecter")
 
-console.log(butt)
+function curren() {
+    const InputcurrencyValue = document.querySelector(".main-input").value
+    const convert = document.querySelector(".valor1")
+    const convertid = document.querySelector(".valor2")
+
+    const dolartoday = 5.2
+    const eurotoday = 6.2
+
+    if (currenselect.value == "Dolar") {
+        convertid.innerHTML = new Intl.NumberFormat("en-US", {
+            
+            style: "currency",
+            currency: "USD"
+
+        }).format(InputcurrencyValue / dolartoday)
+    }
+    if (currenselect.value == "Euro") {
+        convertid.innerHTML = new Intl.NumberFormat("de-DE", {
+
+            style: "currency",
+            currency: "EUR"
+
+        }).format(InputcurrencyValue / eurotoday)
+    }
+    convert.innerHTML = new Intl.NumberFormat("pt-BR", {
+
+        style: "currency",
+        currency: "BRL"
+
+    }).format(InputcurrencyValue)
+}
+
+btnbutoon.addEventListener("click", curren)
